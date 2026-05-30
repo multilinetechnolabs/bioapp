4eb18a782d00ba0ab81ed3f8df3f75956b2dfdc9
multilinetechnolabs <multilinetechnolabs@gmail.com>
2026-05-30 14:56:10 +0530

    add anatomy changes for users

--- resources/views/app/pages/chakrascan/index.blade.php
+++ resources/views/app/pages/chakrascan/index.blade.php
@@ -274,7 +274,6 @@
                                                 border="1">
                                                 <thead></thead>
                                                 <tbody>
-                                                    @if (Auth::user()->isAdmin())
                                                         <tr ng-repeat="label in ctrl.model_labels | orderBy: ctrl.orderBy | filter: { point: { id: model_label.point.id, name: model_label.point.name } } track by label.id"
                                                             ng-if="!(ctrl.client | valPresent)">
                                                             <td colspan="2">
@@ -295,6 +294,7 @@
                                                                         ng-click="ctrl.getCoordinates($event, label.id)">
                                                                         Get Coordinates / Obtener coordenadas
                                                                     </button>
+                                                            @if (Auth::user()->isAdmin())
                                                                     <button type="button"
                                                                         class="modern-btn modern-btn--small modern-btn--outline editor-edit"
                                                                         data-toggle="modal"
