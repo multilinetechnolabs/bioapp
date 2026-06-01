cdc9673ab142f85391d2203b3ad131bac2c00838
multilinetechnolabs <multilinetechnolabs@gmail.com>
2026-06-01 15:52:49 +0530

    change the text color

--- resources/views/app/pages/data_cache/client/body_scan.blade.php
+++ resources/views/app/pages/data_cache/client/body_scan.blade.php
@@ -141,7 +141,7 @@
                         </thead>
                         <tbody ng-repeat="(radical, pairs) in ctrl.displayed_pairs | where: { scan_type: ctrl.scan_type } | filter:{ name: ctrl.searchText } | orderBy: 'radical' | groupBy: 'radical'">
                             <tr>
-                                <td class="header-radical header-radical-<% $index %>" colspan="6"><% radical %></td>
+                                <td class="header-radical header-radical-<% $index %>"  style="color: #000;" colspan="6"><% radical %></td>
                             </tr>
                             <tr class="plain" ng-repeat="pair in pairs | where: { scan_type: ctrl.scan_type } | filter:{ name: ctrl.searchText }  | orderBy: ctrl.sortBy.column track by pair.id">
                                 <td><% pair.name %></td>
